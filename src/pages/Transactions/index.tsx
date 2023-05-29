eac214844b615f5e0e2fcbfa5ce02411db0ac219
MauroBraga <M@r202150016774>
2023-05-29 12:23:28 -0300

    summary

---
+++ src/pages/Transactions/index.tsx
@@ -0,0 +1,11 @@
+import { Header } from "../../components/header";
+import { Summary } from "../../components/Summary";
+
+export function Transaction(){
+    return (
+        <div>
+            <Header />
+            <Summary />
+        </div>
+    )
+}
